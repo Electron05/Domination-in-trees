e13694e93306fbe65cce96693404e8449f920ade
Electron05 <j-czujko@wp.pl>
2026-05-24 12:58:26 +0200

    feat: start implementing parent array converter

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,58 @@
 
 std::ifstream plik("src/nauty/graphs/my_trees.txt");
 
+
+/*
+neighbour list vector<vector<int>>
+
+vector visited = (n,false)
+
+vector parent array = (n,-2)
+
+vector real to parent array index = (n,-1)
+
+
+queue q
+
+
+
+visited[0] = true
+
+parent array[0] = -1
+
+real to parent array index[0] = 0 
+
+int new_id = 0
+while queue.size > 0:
+    cur_old = q.dequeue()
+    par_id = old_to_new[cur_old]
+    for neighbour in neighbour_list[cur_old]: 
+        if !visited[neighbour]
+            new_id++
+            q.enqueue(neighbour)
+            visited[neighbour] = true
+            old_to_new[neighbour] = new_id
+            parent_array[new_id] = par_id*/
+
+void edgesToParentArray(std::string edges){
+	int edgeCount = 1;
+
+	int edgesStrLength = edges.length();
+	for(int i = 0; i < edgesStrLength-1; i++){
+		if(edges[i] == ' ' && edges[i+1] == ' ')
+			edgeCount++;
+	}
+
+	int n = edgeCount+1;
+
+	std::vector<std::vector<int>> neighbourList(n);
+
+	
+
+
+	return;
+}
+
 void openNthTree(int n){
 	int currentTreeIndex = -1;
 
