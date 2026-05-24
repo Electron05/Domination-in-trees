17c238a2a2075636d9ce30a5ec64af16044ad709
Electron05 <j-czujko@wp.pl>
2026-05-24 12:57:24 +0200

    feat: implement tree reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector> 
+
+std::ifstream plik("src/nauty/graphs/my_trees.txt");
+
+void openNthTree(int n){
+	int currentTreeIndex = -1;
+
+	std::string s;
+	s.reserve(75); 
+
+	while(currentTreeIndex < n){
+		std::getline(plik,s);
+		if(!s.empty() && s.at(0) == 'G'){
+			currentTreeIndex++;
+		}
+	}
+	// Skip V E
+	std::getline(plik,s);
+
+	std::string allEdges;
+	// Read edges
+	while(true){
+		std::getline(plik,s);
+		if(s.length()<1) break;
+		allEdges += s + " ";
+		std::cout << "line of edges\n";
+	}
+	std::cout << allEdges;
+	return;
+}
 
 int main(){
-	std::cout << "Hello World!";
+	std::ifstream plik("src/nauty/graphs/my_trees.txt");
+
+	if(!plik.is_open()){
+		return 1;
+	}
+	openNthTree(10);
 	return 0;
 }
